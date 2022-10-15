a225b25aea340ac6379c90e5b3f5dc4eac552f79
Yaa Narki Ankrah <ankrahyaanarki@gmail.com>
2022-10-14 20:57:29 -0700

    Task nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,21 @@
  */
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 48; n < 58; n++)
-
-		putchar(n);
-		if (n != 57)
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+		if (num == 9)
+		{
+			break;
+		}
+		else
 		{
 			putchar(',');
 			putchar(' ');
 		}
-
+	}
 
 	putchar('\n');
 
